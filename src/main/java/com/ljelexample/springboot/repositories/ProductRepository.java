b357ff62c34e2548616b200d5d097c4efef53f5e
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-12-08 16:49:09 -0300

    Ok

---
+++ src/main/java/com/ljelexample/springboot/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.ljelexample.springboot.repositories;
+
+import com.ljelexample.springboot.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+
+}
