21d56bc4dfea56b5ded8539d41b59a43391677ba
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-12-08 10:37:04 -0300

    Project Created and database postgresql connection config.

---
+++ src/main/java/com/ljelexample/springboot/LjexampleApplication.java
@@ -0,0 +1,13 @@
+package com.ljelexample.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LjexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LjexampleApplication.class, args);
+	}
+
+}
